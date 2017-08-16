ee10d44591aab69f520d218650f040df31ed7d29
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-08-16 21:14:18 +0200

    41

---
+++ py/41.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+from primes import Prime
+
+def iter_pandigitals():
+    for n in range(2, 9+1):
+        for x in permutations(list(range(1, n+1))):
+            yield int(''.join(map(str, x)))
+
+def main():
+    prime = Prime()
+    #print(prime.is_prime(987654321))
+    print(max(p for p in iter_pandigitals() if prime.is_prime(p)))
+    #for p in iter_pandigitals():
+    #    print("{}: {}".format(p, prime.is_prime(p)))
+
+main()
---
+++ py/primes/__init__.py
@@ -0,0 +1,31 @@
+import itertools
+
+class Prime(object):
+    def __init__(self):
+        self.primes = [2]
+
+    def is_prime(self, n: int):
+        if n == 1:
+            return False
+        #for p in self.primes:
+        #    if p == n:
+        #        return True
+        #    if n % p == 0:
+        #        return False
+        # 
+        #for i in range(self.primes[-1], n // 2):
+        #    if n % i == 0:
+        #        return False
+        # 
+        #self.primes.append(n)
+        #self.primes.sort()
+        for i in range(2, int(n**0.5) + 1):
+            if n % i==0:
+                return False
+
+        return True
+
+    def iter(self):
+        for i in itertools.count():
+            if self.is_prime(i):
+                yield i
