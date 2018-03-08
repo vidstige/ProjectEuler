ce066351c388b2d8065cf62b41ab726d2132a96e
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-03-08 20:43:46 +1300

    58

--- py/58.py
+++ py/58.py
@@ -1,14 +1,11 @@
+from primes import is_prime
+from itertools import count, islice
+
 # 2 3 5 7
-def spiral(n):
-    sp = {}
+def spirals():
+    #sp = {}
     x, y = 0, 0
     dx, dy = 1, 0
-    names = {
-        (1, 0): 'right',
-        (0, -1): 'up',
-        (-1, 0): 'left',
-        (0, 1): 'down'
-    }
     d = {
         (1, 0): (0, -1),
         (0, -1): (-1, 0),
@@ -23,36 +20,70 @@ def spiral(n):
     }
     s = 0
     c = 0
-    for i in range(1, n * n + 1):
-        if (x, y) in sp:
-            print("ops")
-        sp[(x, y)] = i
+    diagonals = []
+    for i in count(1):
+        #sp[(x, y)] = i
+        if x == y or -x == y:
+            diagonals.append(i)
+
         x, y = x + dx, y + dy
         c += 1
-        #print("{} then {}".format(i, names[(dx, dy)]))
+
         if c > s:
-            # change direction
+            if (dx, dy) == (1, 0):
+                yield diagonals
+                diagonals = []
+
             c = 0
             s += ds[(dx, dy)]
             dx, dy = d[(dx, dy)]
 
-    return sp
-
-def print_spiral(spiral):
-    y_range = range(
-        min(y for _, y in spiral),
-        max(y for _, y in spiral) + 1)
+def get_ranges(spiral):
     x_range = range(
         min(x for _, x in spiral),
         max(x for _, x in spiral) + 1)
+    y_range = range(
+        min(y for _, y in spiral),
+        max(y for _, y in spiral) + 1)
+    return x_range, y_range
+
+def print_spiral(spiral):
+    x_range, y_range = get_ranges(spiral)
     for y in y_range:
         for x in x_range:
             i = spiral.get((x, y), 0)
             print("{: 3}".format(i), end='')
         print()
 
+def diagonal(s):
+    x_range, y_range = get_ranges(s)
+    for x, y in zip(x_range, y_range):
+        yield s[(x, y)]
+    for x, y in zip(reversed(x_range), y_range):
+        yield s[(x, y)]
+
 def main():
-    s = spiral(7)
-    print_spiral(s)
+    #for s, d in islice(spirals(), 3):
+    #    print_spiral(s)
+    #    print(d)
+    #    print(list(sorted(diagonal(s))))
+    #return None
+    
+    #primes = Prime()
+    #is_prime = primes.is_prime
+
+    primes = 0
+    total = 0
+    side_length = 1
+    for diags in spirals():
+        primes += sum(1 for i in diags if is_prime(i))
+        total += len(diags)
+        ratio = primes / total
+        print(" {} {:.4f}".format(side_length, ratio), 
+            end='\r')
+        if ratio < 0.10 and ratio > 0:
+            return
+        side_length += 2
+    print()
 
 main()
--- py/primes/__init__.py
+++ py/primes/__init__.py
@@ -1,5 +1,15 @@
 import itertools
 
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+#@Memoize
 def is_prime(n: int):
     if n <= 1:
         return False
@@ -13,27 +23,35 @@ def is_prime(n: int):
 class Prime(object):
     def __init__(self):
         self.primes = [2]
+        self.cache = {}
 
-    def is_prime(self, n: int):
+    def is_prime(self, n: int) -> bool:
+        if n not in self.cache:
+            self.cache[n] = self._is_prime(n)
+        return self.cache[n]
+
+    def _is_prime(self, n: int) -> bool:
         if n == 1:
             return False
-        #for p in self.primes:
-        #    if p == n:
-        #        return True
-        #    if n % p == 0:
-        #        return False
-        # 
-        #for i in range(self.primes[-1], n // 2):
-        #    if n % i == 0:
-        #        return False
-        # 
-        #self.primes.append(n)
-        #self.primes.sort()
-        for i in range(2, int(n**0.5) + 1):
-            if n % i==0:
+        for p in self.primes:
+            if n == p:
+                return True
+            if n % p == 0:
+                return False
+         
+        for i in range(self.primes[-1] + 1, n // 2):
+            if n % i == 0:
                 return False
 
+        self.primes.append(n)
+        self.primes.sort()
         return True
+
+        #for i in range(2, int(n**0.5) + 1):
+        #    if n % i==0:
+        #        return False
+        # 
+        #return True
 
     def iter(self):
         for i in itertools.count():
